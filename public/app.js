ddea95253c6c109bdecf70376affed073c557c10
Iamneo <techadmin@iamneo.ai>
2023-09-10 08:17:59 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -18,4 +18,45 @@ for (let i = 1; i <= 9; i++) {
   if (playerSymbol === "X")
   playerSymbol = "O"
 else
-  playerSymbol = "X" 
+  playerSymbol = "X"
+  
+  for (let i = 0; i < winPos.length; i++) {
+    if (
+        document.getElementById(winPos[i][0]).innerHTML === playerSymbol &&
+        document.getElementById(winPos[i][1]).innerHTML === playerSymbol &&
+        document.getElementById(winPos[i][2]).innerHTML === playerSymbol
+      ) {
+      
+      }
+      document.getElementById(winPos[i][0]).classList.add("win");
+document.getElementById(winPos[i][1]).classList.add("win");
+document.getElementById(winPos[i][2]).classList.add("win"); 
+gameEnded = true;
+
+setTimeout(function() {
+  alert(playerSymbol + " wins!");
+}, 500);
+
+  }
+  for (let i = 1; i <= 9; i++) {
+    // Whenever a player clicks on a cell
+    document.getElementById(i.toString()).addEventListener(
+      "click", 
+      function() {
+        if (this.innerHTML === "" && !gameEnded) {
+          // Display either "X" or "O" in the cell, and style it
+          this.innerHTML = playerSymbol;
+          this.classList.add(playerSymbol.toLowerCase());
+                  
+          // Check if a player has won
+          checkWin();
+                  
+          // Swap the symbol to the other one for the next turn
+          if (playerSymbol === "X")
+            playerSymbol = "O"
+          else
+            playerSymbol = "X"
+        }
+      }
+    );
+  }
