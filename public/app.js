d01676c0c4af15a99c48dd2d1083633fe00c6af3
Iamneo <techadmin@iamneo.ai>
2023-09-10 08:12:58 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -9,3 +9,13 @@ for (let i = 1; i <= 9; i++) {
       }
     );
   }
+
+  if (this.innerHTML === "" && !gameEnded) {
+    this.innerHTML = playerSymbol;
+  }
+  this.classList.add(playerSymbol.toLowerCase())
+
+  if (playerSymbol === "X")
+  playerSymbol = "O"
+else
+  playerSymbol = "X" 
