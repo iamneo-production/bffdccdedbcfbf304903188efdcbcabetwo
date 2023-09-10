ba8557f08fb045c3ce4adf202c05ba3bef3877b1
Iamneo <techadmin@iamneo.ai>
2023-09-10 07:16:32 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,73 +1,210 @@
-const cells = document.querySelectorAll('[data-cell]');
-const message = document.getElementById('message'); 
-const restartButton = document.getElementById('restart-button'); 
+function CellState() {
+    let state = ' ';
+    const setState = (newState) => {
+        state = newState;
+    };
 
-let currentPlayer = 'X'; 
-let isGameActive = true; 
+    const getState = () => {
+        return state;
+    };
 
-const winningCombinations = [ 
-[0, 1, 2], 
-[3, 4, 5], 
-[6, 7, 8], 
-[0, 3, 6], 
-[1, 4, 7], 
-[2, 5, 8], 
-[0, 4, 8], 
-[2, 4, 6], 
-]; 
-cells.forEach((cell) => { cell.addEventListener('click', handleCellClick); }); 
-restartButton.addEventListener('click', restartGame); 
-function handleCellClick(e) { const cell = e.target; 
-const cellIndex = [...cells].indexOf(cell); 
-
-if (isCellOccupied(cell) || !isGameActive) return; 
-
-cell.textContent = currentPlayer; 
-cell.classList.add(currentPlayer); 
-checkWin(); 
-checkDraw(); 
-currentPlayer = currentPlayer === 'X' ? 'O' : 'X'; 
-message.textContent = `Player ${currentPlayer}'s Turn`; 
-} 
-
-function isCellOccupied(cell) { 
-return cell.classList.contains('X') || 
-cell.classList.contains('O'); 
-} 
-
-function checkWin() { 
-for (const combination of winningCombinations) 
-{ 
-    const [a, b, c] = combination; 
-    
-if ( 
-cells[a].classList.contains(currentPlayer) 
-&& 
-cells[b].classList.contains(currentPlayer) 
-&& 
-cells[c].classList.contains(currentPlayer) 
-) {
- message.textContent = `Player $
- {currentPlayer} wins!`; 
- isGameActive = false; 
-} 
-} 
-} 
-
-function checkDraw() { 
-if ([...cells].every((cell) => 
-cell.classList.contains('X') || 
-cell.classList.contains('O'))) { 
-message.textContent = 'It\'s a draw!'; 
-isGameActive = false; 
-} 
-} 
-function restartGame() { 
-cells.forEach((cell) => { 
-cell.textContent = ''; 
-cell.classList.remove('X', 'O'); 
-}); 
-currentPlayer = 'X'; 
-message.textContent = 'Player X\'s Turn'; 
-isGameActive = true; 
+    return {
+        setState,
+        getState
+    }
 }
+
+
+function GameBoard() {
+    const row = 3;
+    const column = 3;
+    const board = [];
+
+    for (let i = 0; i < row; i++) {
+        board[i] = [];
+        for (let j = 0; j < column; j++)
+            board[i].push(CellState());
+    
+    }
+
+    
+    const makeMove = (row, column, playerSymbol) => {
+        board[row][column].setState(playerSymbol);
+    };
+
+    const printBoard = () => {
+        for (let i = 0; i < row; i++) {
+            let row = '';
+            for (let j = 0; j < column; j++){
+                row += board[i][j].getState() + ' ';
+            }
+            console.log(row);
+        }
+    };
+
+    const deadBoard = () => {
+        for (let i = 0; i < row; i++)
+            for (let j = 0; j < column; j++)
+                if (board[i][j].getState() === ' ') 
+                    return false;
+        return true;
+    };
+
+    const isGameOver = () => {
+        for (let i = 0; i < 3; i++) {
+            if (board[i][0].getState() != ' ' && board[i][0].getState() === board[i][1].getState() && board[i][1].getState() === board[i][2].getState()) return true;
+            if (board[0][i].getState() != ' ' && board[0][i].getState() === board[1][i].getState() && board[1][i].getState() === board[2][i].getState()) return true;
+        }
+        if (board[0][0].getState() != ' ' && board[0][0].getState() === board[1][1].getState() && board[1][1].getState() === board[2][2].getState()) return true;
+        if (board[0][2].getState() != ' ' && board[0][2].getState() === board[1][1].getState() && board[1][1].getState() === board[2][0].getState()) return true;
+        return false;
+    };
+
+
+    const getBoard = () => board;
+
+    return {
+        makeMove,
+        printBoard,
+        getBoard,
+        isGameOver,
+        deadBoard
+    };
+}
+
+
+function Game(playerOne,playerTwo) {
+
+    const board = GameBoard();
+
+
+    const players = [
+        {
+            name: playerOne,
+            symbol: 'X'
+        },
+        {
+            name: playerTwo,
+            symbol: 'O'
+        }
+    ]
+
+    let currentPlayer = players[0];
+    
+    const changePlayer = () => {
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const play = (row, column) => {
+        board.makeMove(row, column, currentPlayer.symbol);
+        changePlayer();
+    };
+
+
+
+    const getWinner = () => {
+        if (board.isGameOver())
+            return currentPlayer === players[0] ? players[1] : players[0];
+        return null;
+    };
+
+    const checkDraw = () => {
+        if (board.deadBoard())
+            return true;
+        return false;
+    };
+
+    const printBoard = () => {  
+        board.printBoard();
+    };
+
+    const getBoard = () => board.getBoard();
+
+    const resetPlayer = () => {
+        currentPlayer = players[0];
+    };
+
+    return {
+        play,
+        getCurrentPlayer,
+        printBoard,
+        getWinner,
+        getBoard,
+        checkDraw,
+        resetPlayer
+    };
+}
+
+
+function GameUI(){
+
+    const game = Game('player 1', 'player 2');
+    const board = game.getBoard();
+
+    const player = document.querySelector('.player-name');
+
+    const render = () => {
+        player.innerHTML = `${game.getCurrentPlayer().symbol}'s turn`;
+        for (let i = 0; i < board.length; i++)
+            for (let j = 0; j < board.length; j++)
+                document.getElementById(`${i}${j}`).innerHTML = board[i][j].getState();
+        
+    };
+
+    const play = () => {
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach(cell => {
+            cell.addEventListener('click', () => {
+                const id = cell.id;
+                const row = parseInt(id[0]);
+                const column = parseInt(id[1]);
+                if (board[row][column].getState() != ' ' || game.getWinner() != null || game.checkDraw())
+                    return;
+                game.play(row, column);
+                render();
+                if (game.getWinner() != null) {
+                    const winner = game.getWinner();
+                    player.innerHTML = `${winner.symbol}'s wins!`;
+                    
+                }
+                
+                else if (game.checkDraw())
+                    player.innerHTML = `It's a draw!`;        
+            
+            });
+        });
+    };
+
+    const reset = () => {
+        for (let i = 0; i < 3; i++)
+            for (let j = 0; j < 3; j++){
+                board[i][j].setState(' ');
+                document.getElementById(`${i}${j}`).innerHTML = ' ';
+            }
+        game.resetPlayer();
+        render();
+    };
+
+    
+    return {
+        play,
+        reset,
+    };
+
+}
+
+const game = GameUI();
+
+const reset = document.querySelector('.reset');
+const play = document.querySelector('.play');
+
+reset.addEventListener('click', () => {
+    game.reset();
+});
+
+play.addEventListener('click', () => {
+    game.reset();
+    game.play();
+});
